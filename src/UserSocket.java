2de1c0e10b5eed87b3854f4b0adf80f902c722a1
afu0706 <a20807506@gmail.com>
2021-03-05 01:25:08 +0800

    add

--- src/UserSocket.java
+++ src/UserSocket.java
@@ -4,13 +4,16 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.IOException;
 import Type.*;
+import DBS.*;
 public class UserSocket extends Thread{
   int           SocketID;
   int           PlayerID = -1;
   Socket        socket;
   InputStream   in;
   OutputStream  out;
+
   byte[]        buf = new byte[1000];
+  boolean       debug = false;         //debug時改成true
 
   public UserSocket(int ID, Socket sc){
     SocketID  = ID;
@@ -26,21 +29,28 @@ public class UserSocket extends Thread{
       in.read(buf);
       ProtocolType data = ProtocolType.ProtocolTrim(buf);
       if (data.protocol == ProtocolType.LOGIN){
-
+        System.out.println(1);
       } else if (data.protocol == ProtocolType.REGISTER){
-
+        System.out.println(2);
       } else {
-
+        System.out.println(3);
       }
 
       //read client action
-      in.read(buf);
-      data = ProtocolType.ProtocolTrim(buf);
-      switch (data.protocol){
+      while (true) {
+        in.read(buf);
+        data = ProtocolType.ProtocolTrim(buf);
+        System.out.println(data.protocol);
+        System.out.println(new String(data.data));
+        //switch (data.protocol) {
+        //}
       }
     }
     catch (Exception e){
-      System.err.println(e);
+      if (debug){
+        System.err.println(e);
+      }
+      //顯示離開ID
       if (PlayerID == -1){
         System.out.printf("Socket ID: %06d out\n", SocketID);
       } else {
