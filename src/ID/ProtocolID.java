f5a2381f55e3efa11e7ee8806a57cec83dab058d
afu0706 <a20807506@gmail.com>
2021-04-22 21:19:41 +0800

    修改協定ID

--- src/ID/ProtocolID.java
+++ src/ID/ProtocolID.java
@@ -6,11 +6,11 @@ public class ProtocolID {
   public static final int REGISTER = 2;
   public static final int SET_NAME = 3;
 
-  public static final int LOGIN_LOCATION = 21;
-  public static final int MOVE = 22;
-  public static final int USE_ITEM = 23;
-  public static final int EQUIP = 24;
-  public static final int DISEQUIP = 25;
-  public static final int ATTACK = 26;
+  public static final int LOGIN_LOCATION = 41;
+  public static final int MOVE = 42;
+  public static final int USE_ITEM = 43;
+  public static final int EQUIP = 44;
+  public static final int DISEQUIP = 45;
+  public static final int ATTACK = 46;
 
 }
