4bd2e5617ba7c187002d7dd03f8691677a1ac83f
Afu0706 <38932724+0975829800@users.noreply.github.com>
2021-10-06 00:28:37 +0800

    GetOtherPlayerInformation

--- src/Action/GetOtherPlayerInformation.java
+++ src/Action/GetOtherPlayerInformation.java
@@ -1,5 +1,6 @@
 package Action;
 
+import DBS.DBConnection;
 import ServerMainBody.Server;
 import Tools.ByteArrayTransform;
 import Type.EquipmentBoxType;
@@ -7,39 +8,34 @@ import Type.PlayerInformation;
 import Type.Status;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.sql.SQLException;
 
 public class GetOtherPlayerInformation {
   public static void get(PlayerInformation p, byte[] data){
     int OtherPID = ByteArrayTransform.ToInt(data,0);
-    for(PlayerInformation playerInformation: Server.Information){
-      if(playerInformation.PID == OtherPID){
-        int start = 0, length = 0;
-        byte[] buf = new byte[Status.SendSize + 16 + 6*EquipmentBoxType.SendSize];
-        byte[] status = playerInformation.status.getByte();
-        System.arraycopy(status,0,buf,0,Status.SendSize);
-        start += Status.SendSize;
-        byte[] Estatus = playerInformation.status.getEquipByte();
-        System.arraycopy(Estatus,0,buf,start,Estatus.length);
-        start += Estatus.length;
-        for(EquipmentBoxType e: playerInformation.equipment){
-          if(e.Equipping){
-            System.arraycopy(e.getByte(),0,buf,start,EquipmentBoxType.SendSize);
-            start += EquipmentBoxType.SendSize;
-            length++;
-          }
+    int start = 0, length = 0;
+    try {
+      DBConnection con = new DBConnection();
+      byte[] buf = new byte[20 + Status.SendSize + 6*EquipmentBoxType.SendSize];
+      byte[] Name = con.getName(OtherPID).getBytes(Charset.forName("big5"));
+      System.arraycopy(Name,0,buf,start,Name.length);
+      start += 20;
+      byte[] status = con.getStatus(OtherPID).getByte();
+      System.arraycopy(status,0,buf,start,Status.SendSize);
+      start += Status.SendSize;
+      for(EquipmentBoxType e: con.getEquipment_bag(OtherPID)){
+        if(e.Equipping){
+          System.arraycopy(e.getByte(),0,buf,start,EquipmentBoxType.SendSize);
+          start += EquipmentBoxType.SendSize;
+          length++;
         }
-        byte[] ans = new byte[Status.SendSize + 16 + length*EquipmentBoxType.SendSize];
-        System.arraycopy(buf,0,ans,0,ans.length);
-
-        try {
-          p.sc.getOutputStream().write(ans);
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
-
-        break;
       }
+      byte[] ans = new byte[Status.SendSize + 16 + length*EquipmentBoxType.SendSize];
+      System.arraycopy(buf,0,ans,0,ans.length);
+      p.sc.getOutputStream().write(ans);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-
   }
 }
